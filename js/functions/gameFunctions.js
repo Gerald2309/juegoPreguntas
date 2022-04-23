cdd2897caf113915b558f6c7879db1310205e6fe
Geraldine <Gerald.e2309@gmail.com>
2022-04-23 04:37:39 -0500

    Alertas y manejo de puntos

--- js/functions/gameFunctions.js
+++ js/functions/gameFunctions.js
@@ -5,29 +5,35 @@ import { levelOne, levelTwo, levelThree, levelFour, levelFive } from '../questio
 export const selectQuestion = (level) => {
   let selectedQuestion = Math.floor(Math.random() * (5 - 0)) + 0;
   let question = {};
+  let reward = 0;
 
   switch (level) {
     case 1:
       question = levelOne[selectedQuestion];
+      reward = 100;
       break;
     case 2:
       question = levelTwo[selectedQuestion];
+      reward = 200;
     case 3:
       question = levelThree[selectedQuestion];
+      reward = 300;
       break;
     case 4:
       question = levelFour[selectedQuestion];
+      reward = 400;
       break;
     case 5:
       question = levelFive[selectedQuestion];
+      reward = 1000;
       break;
   }
 
-  showSelectQuestion(question);
+  showSelectQuestion(question, reward);
 };
 
 // Mostrar pregunta
-export const showSelectQuestion = (question) => {
+export const showSelectQuestion = (question, reward) => {
   let questionContainer = document.querySelector('#game .question');
   let questionOptions = document.querySelector('#game .options');
   let questionElement = document.createElement('h2');
@@ -48,13 +54,18 @@ export const showSelectQuestion = (question) => {
 
   options.forEach((option) => {
     option.addEventListener('click', () => {
-      checkAnswer(option, question);
+      checkAnswer(option, question, reward);
     });
   });
 };
 
 // Verificar respuesta
-export const checkAnswer = (selectedOption, question) => {
+export const checkAnswer = (selectedOption, question, reward) => {
+  let buttons = document.getElementById('buttons');
+  let nextButton = buttons.querySelector('.next-btn');
+  let withdrawButton = buttons.querySelector('.withdraw-btn');
+
+  let points = parseInt(localStorage.getItem('points'));
   let selectedOptionText = selectedOption.textContent;
   let options = document.querySelectorAll('#options li');
 
@@ -64,13 +75,41 @@ export const checkAnswer = (selectedOption, question) => {
 
   if (selectedOptionText === question.answer) {
     selectedOption.classList.add('correct');
+    buttons.classList.add('active');
+    nextButton.classList.add('active');
+    withdrawButton.classList.add('active');
+
+    // Almacenar puntos
+    localStorage.setItem('points', (points += reward));
+
+    // Alerta de respuesta correcta
+    Swal.fire({
+      icon: 'success',
+      title: 'Respuesta correcta',
+      text: 'Ganaste ' + reward + ' puntos',
+      confirmButtonText: 'Entendido',
+    });
   } else {
     selectedOption.classList.add('wrong');
-    // Marcar la respuesta es correcta
+    buttons.classList.add('active');
+    withdrawButton.classList.add('active');
+
+    // Marcar la respuesta que es correcta
     options.forEach((option) => {
       if (option.textContent === question.answer) {
         option.classList.add('correct');
       }
+    });
+
+    // Restablecer puntos
+    localStorage.setItem('points', 0);
+
+    // Alerta de respuesta incorrecta
+    Swal.fire({
+      icon: 'error',
+      title: 'Respuesta incorrecta',
+      text: 'Perdiste todos tus puntos',
+      confirmButtonText: 'Entendido',
     });
   }
 };
