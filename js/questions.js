89522c4be404f6e53cc2355b579077983aee5328
Geraldine <Gerald.e2309@gmail.com>
2022-04-23 01:01:40 -0500

    Se añaden las preguntas del juego

---
+++ js/questions.js
@@ -0,0 +1,146 @@
+export const levelOne = [
+    {
+      question: '¿Cuál es el lugar más frio del planeta tierra?',
+      options: ['Mongolia', 'Kazajistan', 'Estonia', 'La Antártida'],
+      answer: options[3],
+    },
+    {
+      question: '¿Dónde está ubicada la casa blanca?',
+      options: ['París', 'Washington D.C.', 'Roma', 'Cartagena'],
+      answer: options[1],
+    },
+    {
+      question: '¿Dónde nació Leonardo Da Vinci?',
+      options: ['Colombia', 'España', 'Italia', 'Suiza'],
+      answer: options[2],
+    },
+    {
+      question: '¿Cuántos minutos tiene una hora?',
+      options: ['2 Horas', '60 Segundos', '10 Kilometros', '60 minutos'],
+      answer: options[3],
+    },
+    {
+      question: '¿Cuántos huesos tiene el ser humano?',
+      options: ['300', '206', '208', '207'],
+      answer: options[1],
+    },
+  ];
+  
+  export const levelTwo = [
+    {
+      question: '¿Quién escribió La Odisea?',
+      options: ['Nietzsche', 'Piedad Bonnett', 'Homero', 'Tomas Carrasquilla'],
+      answer: options[2],
+    },
+    {
+      question: '¿Quién es el padre del psicoanálista?',
+      options: ['Frederick Starks', 'Lewis', 'Sigmund Freud', 'Merlin'],
+      answer: options[2],
+    },
+    {
+      question: '¿De qué estilo arquitectónico es la Catedral de Notre Dame en París?',
+      options: ['Gotico', 'Clásico', 'Romantico', 'Seductor'],
+      answer: options[0],
+    },
+    {
+      question: '¿Cuál es el libro sagrado del Islam?',
+      options: ['Los sufis', 'Persépolis', 'Sumisión', 'Corán'],
+      answer: options[3],
+    },
+    {
+      question: '¿Qué veterano músico es la canción "Tears in Heaven"?',
+      options: ['Eric Clapton', 'Emmy Winhouse', 'Queen', 'Michael Jackson'],
+      answer: options[0],
+    },
+  ];
+  
+  export const levelThree = [
+    {
+      question: '¿Donde se originaron los juegos olimpicos?',
+      options: ['Estados Unidos', 'Grecia', 'Rusia', 'Finlandia'],
+      answer: options[1],
+    },
+    {
+      question: '¿De qué país es el futbolista Cristiano Ronaldo?',
+      options: ['Belgica', 'Portugal', 'Mongolia', 'Brasil'],
+      answer: options[1],
+    },
+    {
+      question: '¿Cuál es la capital de Hungría?',
+      options: ['Viena', 'Praga', 'Ibagué', 'Budapest'],
+      answer: options[3],
+    },
+    {
+      question: '¿Quién pintó Las meninas?',
+      options: ['Francisco de Goya', 'Diego Velázquez', 'Salvador Dalíç', 'Ronnie Coleman'],
+      answer: options[2],
+    },
+    {
+      question: '¿Cuáles son las flores más famosas pintadas por Van Gogh?',
+      options: ['Flor de loto', 'Girasoles', 'Margaritas', 'Auguste Rodin'],
+      answer: options[1],
+    },
+  ];
+  
+  export const levelFour = [
+    {
+      question: '¿En qué país se encuentra la torre de Pisa?',
+      options: ['Italia', 'Francia', 'Ecuador', 'China'],
+      answer: options[0],
+    },
+    {
+      question: '¿En qué país se encuentra el famoso monumento Taj Mahal?',
+      options: ['Italia', 'India', 'Japón', 'Arabia Saudita'],
+      answer: options[1],
+    },
+    {
+      question: '¿A qué país pertenece la ciudad de Varsovia?',
+      options: ['Catar', 'Berlín', 'Israel', 'Polonia'],
+      answer: options[3],
+    },
+    {
+      question: '¿Qué día celebran los cristianos la festivalidad de la epifania de Jesús?',
+      options: ['8 de Abril', '22 de Marzo', '6 de Enero', '24 de Diciembre'],
+      answer: opntions[2],
+    },
+    {
+      question: 'La campana de Gauss está asociada a:',
+      options: ['El apellido de un físico y matemático', 'la región italiana de Campania', 'el cálculo de probabilidades', 'Todas las anteriors'],
+      answer: opntions[2],
+    },
+  ];
+  
+  export const level5 = [
+    {
+      question:
+        'La relación recíproca positiva que se da entre individuos o poblaciones de dos especies distintas, de la cual ambas se benefician es denominada:',
+      options: ['Depredación', 'Parasitismo', 'Mutualismo', 'Parasitismo'],
+      answer: options[2],
+    },
+    {
+      question: 'Las bacterias y los hongos son ejemplos de:',
+      options: ['Consumidores secundarios', 'Consumidores primarios', 'Productores', 'Descomponedores'],
+      answer: options[3],
+    },
+    {
+      question: 'El conjunto de condiciones físicas y biológicas que son fundamentales para la existencia y la supervivencia de los organismos es denominado:',
+      options: ['Factores bióticos', 'Ambiente', 'Clima', 'Ninguna de las anteriores'],
+      answer: options[1],
+    },
+    {
+      question: 'Las adaptaciones en los seres vivos se clasifican en tres grandes grupos entre las que podemos mencionar:',
+      options: [
+        'Morfológicas, fisiologicas y etológicas',
+        'Genéticas, fisiologicas y etologicas',
+        'Morfologicas, fisiologicas y genéticas',
+        'Ninguna de las anteriores',
+      ],
+      answer: options[0],
+    },
+    {
+      question: 'En nuestro planeta hay gran diversidad de ecosistemas que en su conjunto constituyen lo que se denomina:',
+      options: ['Comunidad', 'Biosfera', 'Globo terráqueo', 'Ninguna de las anteriores'],
+      answer: options[3],
+    },
+  ];
+  
